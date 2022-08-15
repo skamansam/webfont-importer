26cb1a64c5f5433432b94d092ae43750e5f5f4e3
Samuel C Tyler <skamansam@gmail.com>
2022-08-14 23:13:49 -0400

    no jekyll

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,7 +12,14 @@ const config = {
     // customElement: true
   },
 	kit: {
-		adapter: adapter(),
+    paths: {
+      base: process.env.NODE_ENV === "production" ? "/webfont-importer" : "",
+    },
+		adapter: adapter({
+      pages: 'build', 
+      assets: 'build',
+      fallback: 'index.html',
+    }),
 	}
 };
 
